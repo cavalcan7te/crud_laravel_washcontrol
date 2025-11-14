e601159cbc6550debcb9d117e4596b680cb89ed6
cavalcan7te <maria.eduardo9@aluno.ce.gov.br>
2025-11-14 11:53:02 -0300

    feat: adiciona o controller ServiceController.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\CustomerController;
 
 Route::get('/', function () {
@@ -19,6 +20,8 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('customers',CustomerController::class);
+Route::resource('services',ServiceController::class);
+
 
 require __DIR__.'/auth.php';
 
