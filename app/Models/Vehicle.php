de2fdf33afe0ec3f093ac166920c84b335532fd2
cavalcan7te <maria.eduardo9@aluno.ce.gov.br>
2025-12-02 22:26:24 -0300

    feat(vehicle): add model with fillable fields and belongsTo(customer)

---
+++ app/Models/Vehicle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Vehicle extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'plate',
+        'brand',
+        'model',
+        'year',
+        'color',
+        'type',
+        'notes',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
