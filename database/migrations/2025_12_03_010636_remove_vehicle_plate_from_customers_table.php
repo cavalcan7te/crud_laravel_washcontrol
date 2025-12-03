83f250d51a8f984567726bcd978e75012a2822f2
cavalcan7te <maria.eduardo9@aluno.ce.gov.br>
2025-12-02 22:23:02 -0300

    fix(customer): remove deprecated vehicle_plate column from customers table

---
+++ database/migrations/2025_12_03_010636_remove_vehicle_plate_from_customers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('vehicle_plate'); // Remove a coluna
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('customers', function (Blueprint $table) {
+            $table->string('vehicle_plate')->nullable(); // Restaura a coluna caso dê rollback
+        });
+    }
+};
