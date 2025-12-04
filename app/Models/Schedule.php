5c53b60006457000bff6c6d26e287d2e5d73fc93
cavalcan7te <maria.eduardo9@aluno.ce.gov.br>
2025-12-03 21:06:53 -0300

    feat(schedule): define fillable attributes and relationships in Schedule model

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'vehicle_id',
+        'employee_id',
+        'scheduled_at',
+        'service',
+        'price',
+        'status',
+        'notes',
+    ];
+
+    // RELACIONAMENTOS
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
